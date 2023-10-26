444d7936dff6b536e02e10d6625d394ae705753a
Murilo Elias <murilo7456@outlook.com>
2023-10-26 18:11:19 -0300

    Primeiro commit

---
+++ frontend/src/ChatsPage.jsx
@@ -0,0 +1,16 @@
+import {PrettyChatWindow} from 'react-chat-engine-pretty'
+
+const ChatsPage = (props) => {
+    return (
+        <div style={{height: '100vh'}}>
+            <PrettyChatWindow
+                projectId='549ac0e1-aee0-42c4-b62b-0a69ee949366'
+                username={props.user.username}
+                secret={props.user.secret}
+                style={{ height: '100%'}}
+            />
+        </div>
+    )
+}
+
+export default ChatsPage
